4b352d69b80f14db066f2abe7e83de7caa38c4eb
soongjamm <kimstz0303@gmail.com>
2021-03-31 20:24:11 +0900

    refactor: 현재 사용하지 않는 클래스들 정리

--- src/main/java/com/king/burger/accounting/AccountDayDao.java
+++
@@ -1,9 +0,0 @@
-package com.king.burger.accounting;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AccountDayDao extends JpaRepository<AccountDay, Long> {
-
-}
